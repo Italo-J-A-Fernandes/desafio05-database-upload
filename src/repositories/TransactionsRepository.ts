2e177d37f75fdaed7dd1976b1f62f3d32e83d574
Italo Fernandes <italojafernandes@hotmail.com>
2020-11-10 03:57:34 -0300

    Calculating the balance

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -10,9 +10,34 @@ interface Balance {
 
 @EntityRepository(Transaction)
 class TransactionsRepository extends Repository<Transaction> {
-  // public async getBalance(): Promise<Balance> {
-  //   // TODO
-  // }
+  public async getBalance(): Promise<Balance> {
+    const transactions = await this.find();
+
+    const { income, outcome } = transactions.reduce(
+      (acum, tran) => {
+        switch (tran.type) {
+          case 'income':
+            acum.income += Number(tran.value);
+            break;
+          case 'outcome':
+            acum.outcome += Number(tran.value);
+            break;
+          default:
+            break;
+        }
+
+        return acum;
+      },
+      {
+        income: 0,
+        outcome: 0,
+        total: 0,
+      },
+    );
+
+    const total = income - outcome;
+    return { income, outcome, total };
+  }
 }
 
 export default TransactionsRepository;
